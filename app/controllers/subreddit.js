07bffd1dc02d68ad759bf5ce059332e1014dc74b
harris-miller <harrismiller08@gmail.com>
2015-02-19 22:32:28 -0700

    removed use of ObjectController, as it has been deprecated

--- app/controllers/subreddit.js
+++ app/controllers/subreddit.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
 	queryParams: ['t', 'count', 'after', 'before'],
 	t: null,
 	count: null,
