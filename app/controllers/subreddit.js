a44a75efd583cd6a30c4fbd23e08ce061c42ccad
harris-miller <harrismiller08@gmail.com>
2015-02-24 01:22:37 -0700

    subreddit header images are in

--- app/controllers/subreddit.js
+++ app/controllers/subreddit.js
@@ -5,5 +5,16 @@ export default Ember.Controller.extend(PagableMixin, {
 	
 	currentSub: null,
 
-	isFrontpage: false
+	isFrontpage: false,
+
+	hasHeaderImg: Ember.computed.bool('model.about.data.header_img'),
+
+	headerImg: Ember.computed.alias('model.about.data.header_img'),
+
+	headerCoords: Ember.computed('model.about.data.header_size', function() {
+		return {
+			height: this.get('model.about.data.header_size')[1],
+			width: this.get('model.about.data.header_size')[0]
+		};
+	})
 });
