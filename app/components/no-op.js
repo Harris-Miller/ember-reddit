687f35e0d484dedce32a0b0969ae3b485bb2b23f
harris-miller <harrismiller08@gmail.com>
2015-04-05 21:05:20 -0600

    updated ember to 1.11.1, thing.hbs now using component helper instead of if statements (finally!)

---
+++ app/components/no-op.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+// no-op
+// this purposly does nothing
+// it is here currently to satisfy a switch statement in models/thing
+// so the app doesn't crash when using the component helper the switch returns for
+// until the proper components for t2, t4, and t5 are written (if needed)
+export default Ember.Component.extend({
+});
--- app/models/thing.js
+++ app/models/thing.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
+
+	// thing kind logic
 	isT1: Ember.computed('kind', function() {
 		return this.get('kind') === 't1';
 	}),
@@ -19,5 +21,18 @@ export default Ember.Object.extend({
 
 	isT5: Ember.computed('kind', function() {
 		return this.get('kind') === 't5';
+	}),
+
+	// thing kind component enum for Ember 1.11 component helper
+	thingComponent: Ember.computed('kind', function() {
+		switch (this.get('kind'))
+		{
+			case 't1': return 't1-row';
+			case 't3': return 't3-row';
+			case 't2':
+			case 't4':
+			case 't5': return 'no-op';
+			default: return 'no-op';
+		}
 	})
 });
